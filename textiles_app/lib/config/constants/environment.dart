3a76eda8ee485131c04d00b6a135e95200717483
ElianHuanca <elian_huanca@hotmail.com>
2024-04-24 21:31:46 -0400

    nose que cambios hice

--- textiles_app/lib/config/constants/environment.dart
+++ textiles_app/lib/config/constants/environment.dart
@@ -6,7 +6,7 @@ class Environment {
     await dotenv.load(fileName: '.env');
   }
 
-  static String apiUrl = dotenv.env['API_URL'] ?? 'No está configurado el API_URL';
+  static String apiUrl = dotenv.env['API_URL'] ?? 'https://textilesbackend-production.up.railway.app/api';
 
 }
 
